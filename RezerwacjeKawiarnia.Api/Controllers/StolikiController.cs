4cadb698167974bd444647727b2db20da1ef21ca
GrzegorzPolewczak <148885875+GrzegorzPolewczak@users.noreply.github.com>
2026-03-06 21:44:42 +0100

    Dodanie kontrolera i swaggera. Usuniecie pogodynki

---
+++ RezerwacjeKawiarnia.Api/Controllers/StolikiController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RezerwacjeKawiarnia.Api.Data;
+using RezerwacjeKawiarnia.Api.Entities;
+
+namespace RezerwacjeKawiarnia.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StolikiController : ControllerBase
+    {
+        private readonly KawiarniaDbContext _context;
+        public StolikiController(KawiarniaDbContext context) {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Stolik>>> PobierzWszystkie() {
+            var stoliki = await _context.Stoliki.ToListAsync();
+            return Ok(stoliki);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> DodajStolik(Stolik nowyStolik)
+        {
+            _context.Stoliki.Add(nowyStolik);
+            await _context.SaveChangesAsync();
+            return Ok(nowyStolik);
+        }
+    }
+}
---
+++ RezerwacjeKawiarnia.Api/Migrations/20260306191528_InicjalizacjaBazy.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RezerwacjeKawiarnia.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class InicjalizacjaBazy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Stoliki",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Numer = table.Column<int>(type: "int", nullable: false),
+                    LiczbaMiejsc = table.Column<int>(type: "int", nullable: false),
+                    CzyZarezerwowany = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Stoliki", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Stoliki");
+        }
+    }
+}
