173e835555b9454680576bd22b2248a56615b835
GrzegorzPolewczak <148885875+GrzegorzPolewczak@users.noreply.github.com>
2026-03-06 20:05:18 +0100

    Dodanie paczek EF Core, model Stolik, DbContext, ConnectionString z zaslepka na haslo

---
+++ RezerwacjeKawiarnia.Api/Data/KawiarniaDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RezerwacjeKawiarnia.Api.Entities;
+
+namespace RezerwacjeKawiarnia.Api.Data
+{
+    public class KawiarniaDbContext : DbContext
+    {
+        public KawiarniaDbContext(DbContextOptions<KawiarniaDbContext> options) : base(options) { }
+
+        public DbSet<Stolik> Stoliki { get; set; }
+    }
+}
---
+++ RezerwacjeKawiarnia.Api/Entities/Stolik.cs
@@ -0,0 +1,10 @@
+﻿namespace RezerwacjeKawiarnia.Api.Entities
+{
+    public class Stolik
+    {
+        public int Id { get; set; } // Klucz główny
+        public int Numer { get; set; } // Numer stolika
+        public int LiczbaMiejsc {  get; set; } // Liczba miejsc przy stoliku
+        public bool CzyZarezerwowany { get; set; } // Status
+    }
+}
