5bfe8237932548ee8a6e4cd79fe6c072b53ae689
GrzegorzPolewczak <148885875+GrzegorzPolewczak@users.noreply.github.com>
2026-03-11 21:03:49 +0100

    Dodanie walidacji do modelu Stolik

--- RezerwacjeKawiarnia.Api/Entities/Stolik.cs
+++ RezerwacjeKawiarnia.Api/Entities/Stolik.cs
@@ -1,9 +1,15 @@
-﻿namespace RezerwacjeKawiarnia.Api.Entities
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RezerwacjeKawiarnia.Api.Entities
 {
     public class Stolik
     {
         public int Id { get; set; } // Klucz główny
+        [Required(ErrorMessage ="Numer stolika jest wymagany.")]
+        [Range(1,int.MaxValue, ErrorMessage = "Numer stolika musi być dodatni.")]
         public int Numer { get; set; } // Numer stolika
+        [Required(ErrorMessage = "Liczba miejsc jest wymagana.")]
+        [Range(1,int.MaxValue, ErrorMessage = "Liczba miejsc musi być dodatnia.")]
         public int LiczbaMiejsc {  get; set; } // Liczba miejsc przy stoliku
         public bool CzyZarezerwowany { get; set; } // Status
     }
